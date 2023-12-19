9fca71ee534b24cb1c975a9c7b3c9cbb5ecd591a
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-12-19 21:07:14 +0000

    creating question card

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,10 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Componebts
+import QuestionCard from './components/question_card';
+
+const App = () => {
+
+  const startQuiz = async () => {
+
+  }
+
+  const checkAns = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQ = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQ}>Next Question</button>
     </div>
   );
 }
---
+++ my-app/src/components/question_card.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const q_card = () => <div>Question Card</div>;
+
+export default q_card;
