78f212d2ceb4b499acd5e2d4f1bc64b0bfca51e7
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-12-19 21:19:15 +0000

    working on q_cards

--- my-app/src/components/question_card.tsx
+++ my-app/src/components/question_card.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const q_card = () => <div>Question Card</div>;
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAns: string;
+    q_number: number;
+    total_questions: number;
+}
+
+const q_card: React.FC<Props> = ({question, answer, callback, userAns, q_number, total_questions}) => (
+    <div>
+        <p>Question Number: {q_number} / {total_questions}</p>
+    </div>
+);
 
 export default q_card;
